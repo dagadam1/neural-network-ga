2d627dc871a51519bd34316e34ab9801a084a726
Adam Olsson <dagadamsven@gmail.com>
2022-12-18 19:33:58 +0100

    Add log and make program better

--- src/run_network_test.py
+++ src/run_network_test.py
@@ -1,9 +1,21 @@
 import network
 import mnist_loader
 
-network.random.seed(31415)
-network.np.random.seed(31415)
+# Hyper-parameters
+epochs = 30
+mini_batch_size = 10
+eta = 3.0
+
+random_seed = 31415
+network.random.seed(random_seed)
+network.np.random.seed(random_seed)
+
+def train_net(sizes):
+    net = network.Network(sizes)
+    net.SGD(training_data, epochs, mini_batch_size, eta, test_data=test_data)
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
