bfde50b115ccb64b3931ba9fa7faebb95a780df1
Adam Olsson <dagadamsven@gmail.com>
2022-12-18 19:44:30 +0100

    Finish train_net function

--- src/run_network_test.py
+++ src/run_network_test.py
@@ -2,7 +2,7 @@ import network
 import mnist_loader
 
 # Hyper-parameters
-epochs = 30
+epochs = 3
 mini_batch_size = 10
 eta = 3.0
 
@@ -10,12 +10,18 @@ random_seed = 31415
 network.random.seed(random_seed)
 network.np.random.seed(random_seed)
 
-def train_net(sizes):
-    net = network.Network(sizes)
+def train_net(hidden_layer_size):
+    """Train a network consitsting of one input layer with 784 neurons,
+    one output layer with 10 neurons, and one hidden layer
+    with 'hidden_layer_size' neurons.
+
+    Args:
+        hidden_layer_size (int): number of neurons in the hidden layer
+    """
+    
+    net = network.Network([784, hidden_layer_size, 10])
     net.SGD(training_data, epochs, mini_batch_size, eta, test_data=test_data)
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+train_net(30)
