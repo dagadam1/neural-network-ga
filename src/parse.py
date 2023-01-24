72b9078aae0889881792153e02afc3e53d2a7ee3
Adam Olsson <dagadamsven@gmail.com>
2023-01-24 16:42:43 +0100

    Ability to parse multiple runs

--- src/parse.py
+++ src/parse.py
@@ -27,13 +27,30 @@ def main():
     filename = sys.argv[1]
     f = open(filename)
     content = f.read()
-    chunks = content.split("#")
-    #Remove unnecessary lines
-    nets = [i for i in chunks if i != ".\n" and i != ""][1:]
-    for net in nets:
-        #Print first line i.e. the size of the network
-        print(net.split('\n', 1)[0])
-        print(find_max_epoch_value(net))
+    
+    #Find runs
+    runs = content.split("##")
+    #Only keep necessary lines and remove dots, for example
+    runs = [i for i in runs if i != ".\n" and i != ""][1:] 
+    
+    for run in runs:
+        #Print from index 18 in the string of this run, to the first newline character.
+        #This is the info about this run.
+        print(run[18:run.find("\n")])
+        #Find network tests
+        chunks = run.split("#")
+        #Only keep necessary lines and remove dots, for example
+        nets = [i for i in chunks if i != ".\n" and i != ""][1:]
+        for net in nets:
+            #Some of the lines in 'nets' are the timers from the previous net
+            #Check if this line is a timer and if so, print it and continue
+            if net.find("(Time: ") != -1:
+                print(net[2:])
+                continue
+            
+            #Print first line i.e. the size of the network
+            print(net.split('\n', 1)[0])
+            print(find_max_epoch_value(net))
     
     
     
