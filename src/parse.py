230326f69562c9e2610084e376cb38d8a88c86ad
Adam Olsson <dagadamsven@gmail.com>
2023-01-24 18:07:26 +0100

    Make summary more practical

--- src/parse.py
+++ src/parse.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 
 def find_max_epoch_value(net: str):
     """Takes the output from a network from the test program and
@@ -20,12 +20,10 @@ def find_max_epoch_value(net: str):
     epoch_values = [int(epoch.split(" ")[2]) for epoch in epochs]
         
     return max(epoch_values)
-    
-
 
 def main():
-    #List of every run as a dictionary cotaining all the networks and their highest epochs.
-    summary: list[dict[str, int]] =[]
+    #Dictionary cotaining all the types of networks and a list of their highest epochs.
+    summary: dict[str, list[int]] = {}
     
     filename = sys.argv[1]
     f = open(filename)
@@ -34,12 +32,12 @@ def main():
     #Find runs
     runs = content.split("##")
     #Only keep necessary lines and remove dots, for example
-    runs = [i for i in runs if i != ".\n" and i != ""][1:] 
+    runs = [i for i in runs if i != ".\n" and i != ""][0:] 
     
     for run in runs:
         #Print from index 18 in the string of this run, to the first newline character.
         #This is the info about this run.
-        print(run[18:run.find("\n")])
+        print(run[18:run.find("\n")], end="\n\n")
         #Find network tests
         chunks = run.split("#")
         #Only keep necessary lines and remove dots, for example
@@ -58,12 +56,15 @@ def main():
             max_epoch_value = find_max_epoch_value(net)
             
             #Add this network's highest epoch to the summary
-            summary.append({size: max_epoch_value})
+            if size in summary.keys():
+                summary[size].append(max_epoch_value)
+            else:
+                summary[size] = [max_epoch_value]
             
             print(size)
             print(find_max_epoch_value(net))
     
-    
+    print(summary)
     
 
 if __name__ == "__main__":
